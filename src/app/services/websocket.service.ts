0766813ec4623e76946be32e7a1b77fed5605a99
Fabrizzio Lopez <fabroz.dev@gmail.com>
2025-10-23 07:44:28 +0200

    feat: removed dead code and connecting to binance

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,12 +1,25 @@
 import { Injectable, NgZone } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-import { Observable, catchError, EMPTY, retry, shareReplay } from 'rxjs';
+import { Observable, catchError, EMPTY, retry, shareReplay, tap } from 'rxjs';
 
 export interface MarketData {
+  e: 'trade'; // Event type
+  E: number; // Event time (timestamp in milliseconds)
+  s: string; // Symbol (e.g., "BTCUSDT", "BNBBTC")
+  t: number; // Trade ID
+  p: string; // Price (as string to preserve precision)
+  q: string; // Quantity (as string to preserve precision)
+  b: number; // Buyer order ID
+  a: number; // Seller order ID
+  T: number; // Trade time (timestamp in milliseconds)
+  m: boolean; // Is the buyer the market maker?
+  M: boolean; // Ignore (can be used for future enhancements)
+}
+/* {
   symbol: string;
   price: number;
   time: number;
-}
+}*/
 
 @Injectable({ providedIn: 'root' })
 export class WebsocketService {
@@ -14,13 +27,14 @@ export class WebsocketService {
 
   constructor(private ngZone: NgZone) {}
 
-  connect(url = 'ws://localhost:8085'): Observable<MarketData> {
+  connect(url = 'wss://stream.binance.com:9443/ws/btcusdt@trade'): Observable<MarketData> {
     // create inside ngZone.runOutsideAngular if heavy CPU is expected
     if (!this.socket$ || this.socket$.closed) {
       this.socket$ = webSocket({ url });
     }
 
     return this.socket$.pipe(
+      tap((message) => console.log('Received message:', message)),
       retry({ count: Infinity, delay: 2000 }),
       catchError((err) => {
         console.error('WS error', err);
